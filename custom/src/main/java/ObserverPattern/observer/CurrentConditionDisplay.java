17b7f460670da61fed0868dec673e0ddbe5437df
David Crespo <davidcrespoarroyo@gmail.com>
2016-07-24 22:07:03 +0100

    Added CurrentConditionDisplay observer

---
+++ custom/src/main/java/ObserverPattern/observer/CurrentConditionDisplay.java
@@ -0,0 +1,33 @@
+package ObserverPattern.observer;
+
+import ObserverPattern.DisplayElement;
+import ObserverPattern.subject.WeatherData;
+
+/**
+ * Created by davicres on 24/07/2016.
+ */
+public class CurrentConditionDisplay implements Observer, DisplayElement {
+    private final WeatherData weatherData;
+    private float temp;
+    private float humidity;
+
+
+    public CurrentConditionDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void display() {
+        System.out.printf("CurrentConditionDisplay{" +
+                "temp=" + temp +
+                ", humidity=" + humidity +
+                '}');
+    }
+
+    public void update(float temp, float humidity, float pressure) {
+        this.temp = temp;
+        this.humidity = humidity;
+        display();
+    }
+
+}
