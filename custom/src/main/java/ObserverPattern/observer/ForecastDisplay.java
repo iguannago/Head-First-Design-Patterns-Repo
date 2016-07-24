4c6d03ee0b2018e4b2c0fe1af6ef5737a693aaf7
David Crespo <davidcrespoarroyo@gmail.com>
2016-07-24 22:21:01 +0100

    Added ForecastDisplay

---
+++ custom/src/main/java/ObserverPattern/observer/ForecastDisplay.java
@@ -0,0 +1,33 @@
+package ObserverPattern.observer;
+
+import ObserverPattern.DisplayElement;
+import ObserverPattern.subject.WeatherData;
+
+/**
+ * Created by davicres on 24/07/2016.
+ */
+public class ForecastDisplay implements Observer, DisplayElement {
+    private final WeatherData weatherData;
+    private float currentPressure = 29.92f;
+    private float lastPressure;
+
+    public ForecastDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void display() {
+        System.out.println("Forecast: ");
+        if (currentPressure > lastPressure)
+            System.out.println("Improving weather on the way");
+        else if (currentPressure == lastPressure)
+            System.out.println("More of the same");
+        else if (currentPressure < lastPressure)
+            System.out.println("Watch out for cooler, rainy weather");
+    }
+
+    public void update(float temp, float humidity, float pressure) {
+        lastPressure = currentPressure;
+        currentPressure = pressure;
+    }
+}
